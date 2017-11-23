4aedc2e3139ff489329cd482756b5401474bc605
CyanCoco <concyan@outlook.com>
2017-11-23 20:40:56 +0800

    初建server项目，把Mirror项目的server部分分离出来，成为一个单一模块，以便更加优化server部分代码，更好地学习node部分的功能

---
+++ server/Study.js
@@ -0,0 +1,9 @@
+let express = require('express'),
+    Router = express.Router();
+let study = require('./mock/study/study');
+Router.get('/api/studydata', function (req, res) {
+    //获取所有的学习数据，学习页面的首页显示了数据的一部分（前端页面把数据截取一部分即可），点击更多即显示所有的数据即可
+    res.json(study);
+});
+
+module.exports = Router;
